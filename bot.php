d39185a4f5300995aea1e365b999ec739302c37d
DESKTOP-HNH0TRA\PokSitsak <sittisag@hotmail.com>
2017-03-21 22:27:05 +0700

    no message

--- bot.php
+++ bot.php
@@ -1,8 +1,43 @@
 <?php
 
-function void test(){
-	
+	function get_oauth2_token() {
+    global $client_id ='tracking2015';
+    global $client_secret='W3b4p1';
+
+    $oauth2token_url = "https://webapi.forthtrack.com/tracking_authentication/token";
+    $clienttoken_post = array(
+    "client_id" => $client_id,
+    "client_secret" => $client_secret,
+    "username" => "forthtrack",
+    "password" => "tracking",
+    "grant_type" => "password"
+    );
+
+    $curl = curl_init($oauth2token_url);
+
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $clienttoken_post);
+    curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+    $json_response = curl_exec($curl);
+    curl_close($curl);
+
+    $authObj = json_decode($json_response);
+
+    if (isset($authObj->refresh_token)){
+        //refresh token only granted on first authorization for offline access
+        //save to db for future use (db saving not included in example)
+        global $refreshToken;
+        $refreshToken = $authObj->refresh_token;
+    }
+
+    $accessToken = $authObj->access_token;
+    return $accessToken;
 }
+
+echo get_oauth2_token();
 
 
 
