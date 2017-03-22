a4a7f01d2204f4662902c7fd869fc812683b2426
DESKTOP-HNH0TRA\PokSitsak <sittisag@hotmail.com>
2017-03-22 17:53:16 +0700

    no message

--- oath.php
+++ oath.php
@@ -48,7 +48,8 @@ function getVehicleStatus($replyToken)
 
 			//$ch = curl_init($url);
 			$ch = curl_init($url);
-            curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);    
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);    
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
             $result = curl_exec($ch);
             curl_close($ch);
