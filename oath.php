49e1110f03ea5741553357a3aa0634a20a3734b2
DESKTOP-HNH0TRA\PokSitsak <sittisag@hotmail.com>
2017-03-22 17:35:21 +0700

    no message

--- oath.php
+++ oath.php
@@ -38,11 +38,37 @@
     return $accessToken;
 }
 
+
+function getVehicleStatus($replyToken)
+{
+     // Make a POST Request to Messaging API to reply to sender
+			$url = 'https://webapi.forthtrack.com/trackingresource/api/line/'.$replyToken;
+
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
+}
+
+
+
 if(!isset($_COOKIE["accessToken"])) {
     echo get_oauth2_token();
 } else {
     echo "accessToken :'" . $_COOKIE["accessToken"] . "' is set!<br>";
     echo "refreshToken: " . $_COOKIE["refreshToken"];
+}
+
+if($_COOKIE["accessToken"] != "")
+{
+	getVehicleStatus(1111);
 }
 
 
