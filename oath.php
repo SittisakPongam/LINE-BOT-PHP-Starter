b21c044410ddb6bd229a865fa1c2cedb819e3475
DESKTOP-HNH0TRA\PokSitsak <sittisag@hotmail.com>
2017-03-22 17:43:55 +0700

    no message

--- oath.php
+++ oath.php
@@ -46,13 +46,16 @@ function getVehicleStatus($replyToken)
 
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
+			//$ch = curl_init($url);
+			$ch = curl_init();
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);    
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_URL, $url);
+            $result = curl_exec($ch);
+            curl_close($ch);
+
+            //$obj = json_decode($result);			
 
 			echo $result . "\r\n";
 }
