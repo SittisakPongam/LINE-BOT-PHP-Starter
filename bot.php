bfbf9d939125f5b152cade8bd7d5d0c9f4cf0017
DESKTOP-HNH0TRA\PokSitsak <sittisag@hotmail.com>
2017-03-23 17:57:22 +0700

    no message

--- bot.php
+++ bot.php
@@ -26,7 +26,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text.$replyToken.$userId;
+				'text' => $text.$replyToken.$userId
 			];
 
 
