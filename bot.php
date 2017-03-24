568c5decbc968d1778ac80de3728d1b114c6ae46
DESKTOP-HNH0TRA\PokSitsak <sittisag@hotmail.com>
2017-03-24 09:39:30 +0700

    no message

--- bot.php
+++ bot.php
@@ -36,7 +36,7 @@ if (!is_null($events['events'])) {
 			
 			 $messages = [
 				'type' => 'text',
-				'text' => $text.$userId.$result11.$accessToken
+				'text' => $text.$userId.$result11.$authen[0]
 			];
 					
 
