e0b7bb05e9bce6a74ec008555443a8cd1e6791f1
DESKTOP-HNH0TRA\PokSitsak <sittisag@hotmail.com>
2017-04-04 15:24:12 +0700

    no message

--- bot.php
+++ bot.php
@@ -83,108 +83,14 @@ if (!is_null($events['events'])) {
 
 
 
-           // $myText = getWepAPI($userId,$text);
-
-/*
-         $confirm=[  
-  'type' => 'template',
-  'altText' => 'this is a confirm template',
-  'template' =>[ 
-      'type' => 'confirm',
-      'text' => 'Are you sure?',
-      'actions' => [
-          
-            'type' => 'message',
-            'label' => 'Yes',
-            'text' => 'yes'
-          ,
-          
-            'type' =>'message',
-            'label' => 'No',
-            'text' => 'no'          
-      ]
-  ];
-
-  */
-
-
-            //set reply text format
-
-/*
-
-            $action1 =[
-            		'type'  =>'postback',
-                    'label' =>'Check',
-                    'data' => 'check'
-            ];
-
-            
-
-            $action2 =[
-            		'type'  =>'postback',
-                    'label' =>'List',
-                    'data' => 'list'
-            ];
-
-
-            $template = [
-                      'type' => 'buttons',
-                     //'thumbnailImageUrl' => 'https://example.com/bot/images/image.jpg',
-                      'title' => 'Menu',
-                      'text' => 'Please select',
-                      'actions' => [$action1]
-            ];
-
-            
-
-             $myStr = [
-				'type' => 'template',
-				'altText' => 'this is a buttons template',
-				'template' => [$template]
-			];
-
-	*/
-		
-
-/*
-{
-  "type": "template",
-  "altText": "this is a buttons template",
-  "template": {
-      "type": "buttons",
-      "thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
-      "title": "Menu",
-      "text": "Please select",
-      "actions": [
-          {
-            "type": "postback",
-            "label": "Buy",
-            "data": "action=buy&itemid=123"
-          },
-          {
-            "type": "postback",
-            "label": "Add to cart",
-            "data": "action=add&itemid=123"
-          },
-          {
-            "type": "uri",
-            "label": "View detail",
-            "uri": "http://example.com/page/123"
-          }
-      ]
-  }
-}
-*/
+            $myText = getWepAPI($userId,$text);
 
 			
 			 $messages = [
 				'type' => 'text',
-				'text' => 'hello'
+				'text' => $myText
 			];
-
-			
-						
-					
+								
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
