01550c8df010b6938541c09fcaf408da4aab24be
DESKTOP-HNH0TRA\PokSitsak <sittisag@hotmail.com>
2017-08-07 09:05:22 +0700

    no message

--- bot.php
+++ bot.php
@@ -120,11 +120,11 @@ if (!is_null($events['events'])) {
 
             $location =[
 			  {
-			    'type': 'location',
-			    'title': 'my location',
-			    'address': '〒150-0002 東京都渋谷区渋谷２丁目２１−１',
-			    'latitude': 35.65910807942215,
-			    'longitude': 139.70372892916203
+			    'type' => 'location',
+			    'title' => 'my location',
+			    'address'=> '〒150-0002 東京都渋谷区渋谷２丁目２１−１',
+			    'latitude'=> 35.65910807942215,
+			    'longitude'=> 139.70372892916203
 			   }];
 
 			// $messages = [
