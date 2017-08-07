cabc265b8ceef1b5019d4e0df6ea601d180e311c
DESKTOP-HNH0TRA\PokSitsak <sittisag@hotmail.com>
2017-08-07 09:17:56 +0700

    no message

--- bot.php
+++ bot.php
@@ -160,9 +160,10 @@ if (!is_null($events['events'])) {
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
                     //https://api.line.me/v2/bot/message/reply
+
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [$messages],
+				'messages' => $messages,
 			];
 
 			$post = json_encode($data);
