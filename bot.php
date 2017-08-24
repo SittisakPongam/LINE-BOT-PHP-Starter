7214e1ef9e025d78798a6e6398cd2a8180cc824a
DESKTOP-HNH0TRA\PokSitsak <sittisag@hotmail.com>
2017-08-24 17:41:41 +0700

    no message

--- bot.php
+++ bot.php
@@ -215,31 +215,7 @@ if (!is_null($events['events'])) {
 					    ];
 			}
 
-            
-		// 	$confirm = [   'type' => 'template',
-  //     'altText' => 'this is a confirm template',
-  //     'template' => {
-  //     'type' => 'confirm',
-  //     'text' => 'Are you sure?',
-  //     'actions' : [
-  //         {
-  //           'type' => 'message',
-  //           "label": "Yes",
-  //           "text": "yes"
-  //         },
-  //         {
-  //           "type": "message",
-  //           "label": "No",
-  //           "text": "no"
-  //         }
-  //     ]
-  // }];
-
-
-            // set image
-
-		
-								
+			
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
